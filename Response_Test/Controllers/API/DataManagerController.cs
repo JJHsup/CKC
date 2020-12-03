5b82366f7bd9107551e0bed11ca62740871825c4
Jjh <jpsfebk@gmail.com>
2020-12-03 23:20:20 +0800

    Tester Name can be download

---
+++ Response_Test/Controllers/API/DataManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Response_Test.Controllers.API
+{
+    [RoutePrefix("api/[Controller]/[action]")]
+    public class DataManagerController : ApiController
+    {
+        [HttpPost]
+        public void BuildData(dynamic data)
+        {
+        }
+    }
+}
---
+++ Response_Test/Controllers/API/TesterController.cs
@@ -0,0 +1,23 @@
+﻿using Response_Test.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Response_Test.Controllers.API
+{
+    [RoutePrefix("api/[Controller]/[action]")]
+    public class TesterController : ApiController
+    {
+        [HttpGet]
+        public List<string> TesterName()
+        {
+            CatchNowTester name = new CatchNowTester();
+            List<string> result = new List<string>();
+            result.Add(name.Name());
+            return result;
+        }
+    }
+}
